e63f3fb023798c40645acec726c4407c3a5bf41b
LitvinSofia <sofialitvin823@gmail.com>
2025-06-12 19:26:13 +0300

    deleted unused library

--- app/src/db.py
+++ app/src/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, Text, BigInteger
+from sqlalchemy import create_engine, Column, String, Text, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
@@ -7,90 +7,66 @@ import os
 load_dotenv()
 Base = declarative_base()
 
-class Book(Base):
-    __tablename__ = "favorite_book"
-    id = Column(String, primary_key=True)  # ID из Google Books
+class FavoriteBook(Base):
+    __tablename__ = "favorite_books"
+    id = Column(String, primary_key=True)  # ID книги из API
     title = Column(String)
     authors = Column(String)
     description = Column(Text)
     thumbnail_url = Column(String)
-    user_id = Column(BigInteger)
+    user_id = Column(BigInteger)  # ID пользователя Telegram
 
 class Database:
     def __init__(self):
         self.engine = create_engine(os.getenv("DB_URL"))
-        self.Session = sessionmaker(bind=self.engine)
         Base.metadata.create_all(self.engine)
-    
-    def save_book(self, book_data):
-        session = self.Session()
-        book = Book(
-            id=book_data["id"],
-            title=book_data["title"],
-            authors=book_data["authors"],
-            description=book_data["description"],
-            thumbnail_url=book_data.get("thumbnail"),
-            user_id=book_data["user_id"]
-        )
-        session.merge(book)
-        session.commit()
-        session.close()
+        self.Session = sessionmaker(bind=self.engine)
 
-    def get_books(self, user_id):
-        """
-        Возвращает список книг для указанного пользователя
-        
-        Args:
-            user_id (int): ID пользователя Telegram
-            
-        Returns:
-            list: Список словарей с информацией о книгах
-                  [{"id": "...", "title": "...", ...}, ...]
-        """
+    async def add_favorite(self, book_data: dict):
+        """Добавляет книгу в избранное"""
         session = self.Session()
         try:
-            books = session.query(Book).filter_by(user_id=user_id).all()
+            book = FavoriteBook(
+                id=book_data["id"],
+                title=book_data["title"],
+                authors=book_data["authors"],
+                description=book_data.get("description", ""),
+                thumbnail_url=book_data.get("thumbnail"),
+                user_id=book_data["user_id"]
+            )
+            session.merge(book)  # Используем merge вместо add для избежания дубликатов
+            session.commit()
+            return True
+        except Exception as e:
+            session.rollback()
+            raise e
+        finally:
+            session.close()
+
+    async def get_favorites(self, user_id: int):
+        """Получает все избранные книги пользователя"""
+        session = self.Session()
+        try:
+            books = session.query(FavoriteBook).filter_by(user_id=user_id).all()
             return [{
                 "id": book.id,
                 "title": book.title,
                 "authors": book.authors,
                 "description": book.description,
-                "thumbnail": book.thumbnail_url,
-                "user_id": book.user_id
+                "thumbnail": book.thumbnail_url
             } for book in books]
         finally:
             session.close()
 
-# Пример использования
-if __name__ == "__main__":
-    db = Database()
-    
-    # Тестовые данные
-    test_books = [
-        {
-            "id": "book1",
-            "title": "Тестовая книга 1",
-            "authors": "Автор 1",
-            "description": "Описание 1",
-            "thumbnail": "http://example.com/cover1.jpg",
-            "user_id": 12345
-        },
-        {
-            "id": "book2",
-            "title": "Тестовая книга 2",
-            "authors": "Автор 2",
-            "description": "Описание 2",
-            "thumbnail": None,
-            "user_id": 12345
-        }
-    ]
-    
-    # Сохраняем книги
-    for book in test_books:
-        db.save_book(book)
-    
-    # Получаем книги пользователя
-    user_books = db.get_books(12345)
-    print("Найденные книги:")
-    for book in user_books:
-        print(f"{book['title']} by {book['authors']}")
+    async def remove_favorite(self, user_id: int, book_id: str):
+        """Удаляет книгу из избранного"""
+        session = self.Session()
+        try:
+            book = session.query(FavoriteBook).filter_by(id=book_id, user_id=user_id).first()
+            if book:
+                session.delete(book)
+                session.commit()
+                return True
+            return False
+        finally:
+            session.close()
