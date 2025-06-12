61a264dcfe85a468b3301e57061a58edd099ae00
LitvinSofia <sofialitvin823@gmail.com>
2025-06-13 00:32:07 +0300

    added unit tests to bot.py

--- app/src/bot.py
+++ app/src/bot.py
@@ -1,9 +1,9 @@
 import asyncio
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackQueryHandler
 from telegram import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from google_books import GoogleBooksAPI
-from open_lib import OpenLibraryAPI
-from db import Database
+from .google_books import GoogleBooksAPI
+from .open_lib import OpenLibraryAPI
+from .db import Database
 from dotenv import load_dotenv
 import os
 import logging
